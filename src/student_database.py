196ba6f89bbd473cbbd4c5e2c2c8955b7862028b
modengann <99417989+modengann@users.noreply.github.com>
2025-09-04 12:31:43 -0400

    Update student_database.py

--- src/student_database.py
+++ src/student_database.py
@@ -18,23 +18,5 @@ def summary(database: dict):
 
 
 if __name__ == "__main__":
-    students = {}
-    add_student(students, "Peter")
-    add_student(students, "Eliza")
-    print_student(students, "Peter")
-    print_student(students, "Eliza")
-    print_student(students, "Jack")
-
-    add_course(students, "Peter", ("Data Structures and Algorithms", 1))
-    add_course(students, "Peter", ("Introduction to Programming", 1))
-    add_course(students, "Peter", ("Advanced Course in Programming", 1))
-    add_course(students, "Peter", ("Introduction to Programming", 3))
-    add_course(students, "Peter", ("Advanced Course in Programming", 2))
-    add_course(students, "Peter", ("Introduction to Programming", 2))
-
-    add_course(students, "Peter", ("Data Structures and Algorithms", 0))
-
-    add_course(students, "Eliza", ("Introduction to Programming", 5))
-    add_course(students, "Eliza", ("Introduction to Computer Science", 4))
-    print_student(students, "Peter")
-    summary(students)
+    #update this for each part
+    pass
